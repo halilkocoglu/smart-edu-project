bf411fa3dd48c57e650b530967358d8a8ff955ab
Halil Kocoglu <halilkocoglu98@gmail.com>
2023-06-01 16:50:45 +0300

    Updating Links

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require("express");
 
 const app = express();
 
+// template engine
+app.set("view engine", "ejs");
+
+// Middleware
+app.use(express.static("public"));
+
+//Routes
 app.get("/", (req, res) => {
-  res.status(200).send("index");
+  res.status(200).render("index", { pageName: "index" });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about", { pageName: "about" });
 });
 
 const port = 3000;
