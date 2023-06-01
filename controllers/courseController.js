d52ac80279450f21d133b2ae2540a35cfc8258d5
Halil Kocoglu <halilkocoglu98@gmail.com>
2023-06-01 21:32:21 +0300

    Single Course page created.

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,8 +1,8 @@
 const Course = require("../models/Course");
 
-exports.createCourse = async (req, res, next) => {
-  const course = await Course.create(req.body);
+exports.createCourse = async (req, res) => {
   try {
+    const course = await Course.create(req.body);
     res.status(201).json({
       status: "success",
       course,
@@ -10,6 +10,39 @@ exports.createCourse = async (req, res, next) => {
   } catch (error) {
     res.status(400).json({
       status: "bad-request",
+      errorMessage: error.message,
+      error,
+    });
+  }
+};
+
+exports.getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.status(200).render("courses", {
+      courses,
+      pageName: "courses",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Fail",
+      errorMessage: error.message,
+      error,
+    });
+  }
+};
+
+exports.getSingleCourse = async (req, res) => {
+  try {
+    const course = await Course.findOne({ slug: req.params.slug });
+    res.status(200).render("course-single", {
+      course,
+      pageName: "courses",
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "course not found.",
+      errorMessage: error.message,
       error,
     });
   }
