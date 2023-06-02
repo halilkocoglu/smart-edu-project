217697d7f7719ecf2639ca2ed0f864a20256430a
Halil Kocoglu <halilkocoglu98@gmail.com>
2023-06-02 10:55:08 +0300

    Login page created.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
@@ -13,5 +13,23 @@ exports.createUser = async (req, res) => {
       errorMessage: error.message,
       error,
     });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user) {
+      bcrypt.compare(password, user.password, (err, same) => {
+        if (same) {
+          // USER SESSION
+          res.status(200).send("login success");
+        }
+      });
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
   }
 };
