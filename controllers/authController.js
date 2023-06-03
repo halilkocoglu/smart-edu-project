5ed630bfc5a5f9fd74d36c48ccf3d0f3302b1cc5
Halil Kocoglu <halilkocoglu98@gmail.com>
2023-06-03 17:32:17 +0300

    Basic validation with express-validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
+
 const User = require("../models/User");
 const Category = require("../models/Category");
 const Course = require("../models/Course");
@@ -7,11 +9,13 @@ exports.createUser = async (req, res) => {
     const user = await User.create(req.body);
     res.status(201).redirect("/");
   } catch (error) {
-    res.status(400).json({
-      status: "Fail Auth",
-      errorMessage: error.message,
-      error,
-    });
+    const errors = validationResult(req);
+    // console.log(errors.array()[0].msg);
+
+    for (let i = 0; i < errors.array().length; i++) {
+      req.flash("error", `${errors.array()[i].msg} `);
+    }
+    res.status(400).redirect("/register");
   }
 };
 
@@ -23,9 +27,17 @@ exports.loginUser = async (req, res) => {
     if (user) {
       bcrypt.compare(password, user.password, (err, same) => {
         // USER SESSION
-        req.session.userID = user._id;
-        res.status(200).redirect("/auth/dashboard");
+        if (same) {
+          req.session.userID = user._id;
+          res.status(200).redirect("/auth/dashboard");
+        } else {
+          req.flash("error", `Your password is not correct`);
+          res.status(400).redirect("/login");
+        }
       });
+    } else {
+      req.flash("error", `User is not exists`);
+      res.status(400).redirect("/login");
     }
   } catch (error) {
     res.status(400).send(error.message);
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const User = require("../models/User");
 
+const { body } = require("express-validator");
 const authController = require("../controllers/authController");
 const { checkDash } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.route("/signup").post(authController.createUser);
+router.route("/signup").post(
+  [
+    body("name").not().isEmpty().withMessage("Please enter your name"),
+
+    body("email")
+      .isEmail()
+      .withMessage("Please enter valid email")
+      .custom((userEmail) => {
+        return User.findOne({ email: userEmail }).then((user) => {
+          if (user) {
+            return Promise.reject("Email already exists");
+          }
+        });
+      }),
+
+    body("password").not().isEmpty().withMessage("Please enter your password"),
+  ],
+  authController.createUser
+);
 router.route("/login").post(authController.loginUser);
 router.route("/logout").get(authController.logoutUser);
 router.route("/dashboard").get(checkDash, authController.getDashboardPage);
